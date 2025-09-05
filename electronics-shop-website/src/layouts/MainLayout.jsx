e08519fa1751c2db52ec21b2c7cacf87ddcce56b
Przemek Kwiatkowski <kwiatkowskip077@gmail.com>
2025-09-05 09:58:05 +0200

    Add basic style for main layout

--- electronics-shop-website/src/layouts/MainLayout.jsx
+++ electronics-shop-website/src/layouts/MainLayout.jsx
@@ -1,6 +1,8 @@
 import "../components/Header/Header"
 import "../components/Footer/Footer"
 
+import "./MainLayout.css"
+
 import { Outlet } from "react-router-dom"
 
 export default function MainLayout() {
