14e1b5e0612da7b5c19ee5ead325ca9f7afbf41d
Przemek Kwiatkowski <kwiatkowskip077@gmail.com>
2025-09-05 10:03:47 +0200

    Create router file

---
+++ electronics-shop-website/src/router/Router.tsx
@@ -0,0 +1,12 @@
+import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<MainLayout />}>
+
+        </Route>
+    )
+);
+
+export default router;
