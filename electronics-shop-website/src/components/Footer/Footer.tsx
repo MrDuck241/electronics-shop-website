06948ac10069735719684d387e6ff7a04c7e2e12
Przemek <kwiatkowskip077@gmail.com>
2025-09-06 22:59:06 +0200

    Add tailwind css to project

--- electronics-shop-website/src/components/Footer/Footer.tsx
+++ electronics-shop-website/src/components/Footer/Footer.tsx
@@ -4,6 +4,7 @@ export default function Footer() {
     return (
         <footer>
             <span>This is footer</span>
+            <span className="text-green-500 bg-yellow-700">TAILWIND CSS TEXT</span>
         </footer>
     )
 }
