891f003af9560d92e1fe0ef693860b4da5777f1f
Przemek <kwiatkowskip077@gmail.com>
2025-09-07 23:12:24 +0200

    Add main section and background

---
+++ electronics-shop-website/src/pages/Homepage/Homepage.tsx
@@ -0,0 +1,13 @@
+import "./Homepage.css"
+
+const Homepage = () => {
+    return (
+        <div className="homepage-container w-full flex flex-col">
+            <div className="h-[800px] w-[70%] bg-black ml-[200px] mt-5 mb-5 opacity-65">
+                <span className="text-cyan-400 text-3xl">MAIN CONTENT</span>
+            </div>
+        </div>
+    )
+}
+
+export default Homepage
--- electronics-shop-website/src/router/Router.tsx
+++ electronics-shop-website/src/router/Router.tsx
@@ -1,9 +1,11 @@
 import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
+import Homepage from "../pages/Homepage/Homepage"
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<MainLayout />}>
+            <Route index element={<Homepage />} />
         </Route>
     )
 );
