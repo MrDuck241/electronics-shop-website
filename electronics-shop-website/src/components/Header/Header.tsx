3f3b4b0ed88d3211d8714df19bf2f6c26395884f
Przemek <kwiatkowskip077@gmail.com>
2025-09-07 18:46:18 +0200

    Add radix-ui library to project

--- electronics-shop-website/src/components/Header/Header.tsx
+++ electronics-shop-website/src/components/Header/Header.tsx
@@ -1,9 +1,15 @@
 import "./Header.css"
+import { Flex, Text, Button, Slider } from "@radix-ui/themes";
 
 const Header = () => {
     return (
         <header>
             <span>This is header</span>
+            <Flex direction="column" gap="2">
+                <Text>Hello from Radix Themes :)</Text>
+                <Button>Let's go</Button>
+                <Slider defaultValue={[50]} />
+            </Flex>
         </header>
     )
 }
