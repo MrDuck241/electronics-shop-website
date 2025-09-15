afd8ecfb109aa074e50fac2f2914f887e8facc47
Przemek <kwiatkowskip077@gmail.com>
2025-09-16 00:11:42 +0200

    Improve homepage

--- electronics-shop-website/src/pages/Homepage/Homepage.tsx
+++ electronics-shop-website/src/pages/Homepage/Homepage.tsx
@@ -2,9 +2,11 @@ import "./Homepage.css"
 
 const Homepage = () => {
     return (
-        <div className="homepage-container w-[100%] flex flex-col">
-            <div className="h-[800px] w-[70%] bg-black ml-[200px] mt-5 mb-5 opacity-65">
-                <span className="text-cyan-400 text-3xl">MAIN CONTENT</span>
+        <div className="homepage-container w-[100%] flex flex-col justify-center items-center">
+            <div className="h-[800px] w-[70%] bg-black mt-4 mb-4 flex flex-col items-center">
+                <span className="text-cyan-400 text-2xl font-bold mt-3">WELCOME TO ML POWERED RECOMENDATION SHOP</span>
+                <span className="text-white text-xl font-semibold mt-3">Find the best electronics products tailored to your preferences!</span>
+                <img src="images/products/keyboards/keyboard1.jpg" alt="Homepage keyboard image" className="w-[50%] mt-[15px]" />
             </div>
         </div>
     )
