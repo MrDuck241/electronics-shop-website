e7acbdfad679fb6aa5644159265e6ef5769373ef
Przemek <kwiatkowskip077@gmail.com>
2025-09-15 14:03:21 +0200

    Change website title

--- electronics-shop-website/src/components/Footer/Footer.tsx
+++ electronics-shop-website/src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import { Flex, Text, Button, Slider, TextField } from "@radix-ui/themes";
 
 export default function Footer() {
     return (
-        <footer className="w-full h-[120px] bg-gradient-to-r from-black via-gray-900 to-gray-800 flex justify-evenly items-center">
+        <footer className="w-[100%] h-[120px] bg-gradient-to-r from-black via-gray-900 to-gray-800 flex justify-evenly items-center">
             <Text className="text-cyan-400">Author: Przemek K</Text>
             <Text className="text-cyan-400">Webiste in progress</Text>
             <Text className="text-cyan-400">Author: Michal B</Text>
